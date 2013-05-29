9a5cb89b9cf98964b2d320340dae81ee3593c6eb
spiaggefredde <spiaggefredde@gmail.com>
2013-05-29 15:24:57 +0200

    Setting gateway categories - chamgimg HashMap to Hashtable

--- Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/GameWorld.java
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/GameWorld.java
@@ -1,6 +1,8 @@
 package sfs2x.extensions.projectsasha.game.entities;
 
-import java.util.HashMap;
+
+
+import java.util.Hashtable;
 import java.util.Map;
 
 import sfs2x.extensions.projectsasha.game.entities.gateways.*;
@@ -9,54 +11,54 @@ public class GameWorld
 {
 	public int id;
 	public Region[] regions;
-	public static Map<String,Gateway> gateways = new HashMap<String,Gateway>();
+	public static Map<String,Gateway> gateways = new Hashtable<String,Gateway>();
 	
 	public GameWorld(int id)
 	{
 		this.id = id;
 	
-		gateways.put("iceland",				new Base(null, "nome", "Iceland"));
+		gateways.put("iceland",				new Scientific(null, "nome", "Iceland"));
 		gateways.put("great britain",		new Base(null, "nome", "Great Britain"));
-		gateways.put("scandinavia", 		new Base(null, "nome", "Scandinavia"));
-		gateways.put("northern europe",		new Base(null, "nome", "Northern Europe"));
+		gateways.put("scandinavia", 		new Educational(null, "nome", "Scandinavia"));
+		gateways.put("northern europe",		new Bank(null, "nome", "Northern Europe"));
 		gateways.put("ukraine", 			new Base(null, "nome", "Ukraine"));
-		gateways.put("western europe",		new Base(null, "nome", "Western Europe"));
+		gateways.put("western europe",		new Educational(null, "nome", "Western Europe"));
 		gateways.put("southern europe",		new Base(null, "nome", "Southern Europe"));
 		gateways.put("north africa",		new Base(null, "nome", "Northern Europe"));
-		gateways.put("egypt",				new Base(null, "nome", "Egypt"));
+		gateways.put("egypt",				new Military(null, "nome", "Egypt"));
 		gateways.put("east africa",			new Base(null, "nome", "East Africa"));
 		gateways.put("congo",				new Base(null, "nome", "Congo"));
-		gateways.put("madagascar",			new Base(null, "nome", "Madagascar"));
-		gateways.put("south africa",		new Base(null, "nome", "South Africa"));
+		gateways.put("madagascar",			new Bank(null, "nome", "Madagascar"));
+		gateways.put("south africa",		new Educational(null, "nome", "South Africa"));
 		gateways.put("middle east",			new Base(null, "nome", "Middle East"));
-		gateways.put("afghanistan",			new Base(null, "nome", "Afghanistan"));
-		gateways.put("india",				new Base(null, "nome", "India"));
+		gateways.put("afghanistan",			new Military(null, "nome", "Afghanistan"));
+		gateways.put("india",				new Scientific(null, "nome", "India"));
 		gateways.put("siam",				new Base(null, "nome", "Siam"));
-		gateways.put("china",				new Base(null, "nome", "China"));
+		gateways.put("china",				new Government(null, "nome", "China"));
 		gateways.put("ural",				new Base(null, "nome", "Ural"));
 		gateways.put("siberia",				new Base(null, "nome", "Siberia"));
 		gateways.put("mongolia",			new Base(null, "nome", "Mongolia"));
-		gateways.put("irkutsk",				new Base(null, "nome", "Irkutsk"));
+		gateways.put("irkutsk",				new Educational(null, "nome", "Irkutsk"));
 		gateways.put("yakutsk",				new Base(null, "nome", "Yakutsk"));
 		gateways.put("kamchatka",			new Base(null, "nome", "Kamchatka"));
-		gateways.put("japan",				new Base(null, "nome", "Japan"));
+		gateways.put("japan",				new Scientific(null, "nome", "Japan"));
 		gateways.put("indonesia",			new Base(null, "nome", "Indonesia"));
-		gateways.put("western australia", 	new Base(null, "nome", "Western Australia"));
-		gateways.put("eastern australia", 	new Base(null, "nome", "Eastern Australia"));
+		gateways.put("western australia", 	new Government(null, "nome", "Western Australia"));
+		gateways.put("eastern australia", 	new Bank(null, "nome", "Eastern Australia"));
 		gateways.put("new guinea", 			new Base(null, "nome", "New Guinea"));
-		gateways.put("alaska", 				new Base(null, "nome", "Alaska"));
+		gateways.put("alaska", 				new Scientific(null, "nome", "Alaska"));
 		gateways.put("north west territory",new Base(null, "nome", "North West Territory"));
 		gateways.put("alberta", 			new Base(null, "nome", "Alberta"));
-		gateways.put("western usa", 		new Base(null, "nome", "Western USA"));
-		gateways.put("eastern usa", 		new Base(null, "nome", "Eastern USA"));
+		gateways.put("western usa", 		new Bank(null, "nome", "Western USA"));
+		gateways.put("eastern usa", 		new Military(null, "nome", "Eastern USA"));
 		gateways.put("ontario", 			new Base(null, "nome", "Ontario"));
 		gateways.put("central america", 	new Base(null, "nome", "Central America"));
 		gateways.put("quebec",				new Base(null, "nome", "Quebec"));
-		gateways.put("greenland",			new Base(null, "nome", "Greenland"));
+		gateways.put("greenland",			new Government(null, "nome", "Greenland"));
 		gateways.put("venezuela",			new Base(null, "nome", "Venezuela"));
 		gateways.put("peru",				new Base(null, "nome", "peru"));
-		gateways.put("brazil",				new Base(null, "nome", "Brazil"));
-		gateways.put("argentina",			new Base(null, "nome", "Argentina"));	
+		gateways.put("brazil",				new Military(null, "nome", "Brazil"));
+		gateways.put("argentina",			new Government(null, "nome", "Argentina"));	
 		
 		gateways.get("iceland").setNeighborhoods(new Gateway[]
 		{
