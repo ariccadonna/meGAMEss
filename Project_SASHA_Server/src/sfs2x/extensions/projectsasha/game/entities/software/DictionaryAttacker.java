252f31b8a597b1e347821d9f572eec0b4ccdc409
UncleSam89 <samuele.degiuseppe@gmail.com>
2013-06-04 18:37:03 +0300

    Update DictionaryAttacker.java

--- Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/software/DictionaryAttacker.java
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/software/DictionaryAttacker.java
@@ -12,6 +12,8 @@ public class DictionaryAttacker extends Software
 		super(GameConsts.DICTIONARY_NAME, 1);
 		setCumulative(GameConsts.DICTIONARY_CUMULATIVE);
 		setType(GameConsts.DICTIONARY);
+		setDescription(GameConsts.DICTIONARY_DESCRIPTION);
+
 	}
 	
 	
@@ -20,6 +22,8 @@ public class DictionaryAttacker extends Software
 		super(GameConsts.DICTIONARY_NAME, version);
 		setCumulative(GameConsts.DICTIONARY_CUMULATIVE);
 		setType(GameConsts.DICTIONARY);
+		setDescription(GameConsts.DICTIONARY_DESCRIPTION);
+
 	}
 	
 	@Override
