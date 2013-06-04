0c7bcd7d2cd0f658197d58299dac6b124ec5f632
UncleSam89 <samuele.degiuseppe@gmail.com>
2013-06-04 18:04:13 +0200

    Update GameConsts.java

--- Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/GameConsts.java
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/GameConsts.java
@@ -66,6 +66,7 @@ public class GameConsts
 	public static final int FIREWALL_MAX_LEVEL			= 3;
 	public static final boolean FIREWALL_CUMULATIVE		= true;
 	public static final int FIREWALL_PRICE_PERLEVEL		= 10;
+	public static final String FIREWALL_DESCRIPTION 		= "Add a value to defense of gateway";
 	
 	public static final String IDS						= "IDS";
 	public static final String IDS_NAME 				= "Prelude Detection System";
@@ -73,30 +74,37 @@ public class GameConsts
 	public static final int IDS_MAX_LEVEL				= 3;
 	public static final boolean IDS_CUMULATIVE			= false;
 	public static final int IDS_PRICE_PERLEVEL			= 10;
+	public static final String IDS_DESCRIPTION		 	= "The system alerts a player of ana attack when it spent 75% of the hacking time. IDS allows to reduce this percentage";
 	
 	public static final String LOGCLEANER				= "LOGCLEANER";
 	public static final String LOGCLEANER_NAME 			= "LCleaner";
 	public static final int LOGCLEANER_MAX_LEVEL		= 3;
 	public static final boolean LOGCLEANER_CUMULATIVE	= false;
 	public static final int LOGCLEANER_PRICE_PERLEVEL	= 10;
+	public static final String LOGCLEANER_DESCRIPTION		= "Allows to carry out ana attack with a reduced number of track along the path based on a level device";
 	
 	public static final String ANTIVIRUS				= "ANTIVIRUS";
 	public static final String ANTIVIRUS_NAME 			= "Morton Antivirus";
 	public static final int ANTIVIRUS_MAX_LEVEL			= 3;
 	public static final boolean ANTIVIRUS_CUMULATIVE	= false;
 	public static final int ANTIVIRUS_PRICE_PERLEVEL	= 10;
+	public static final String ANTIVIRUS_DESCRIPTION 		= "Allows to protect from a Virus";
+
 	
 	public static final String VIRUS					= "VIRUS";
 	public static final String VIRUS_NAME 				= "Blaster";
 	public static final int VIRUS_MAX_LEVEL				= 3;
 	public static final boolean VIRUS_CUMULATIVE		= false;
 	public static final int VIRUS_PRICE_PERLEVEL		= 10;
+	public static final String VIRUS_DESCRIPTION 		= "When it's equipped on a gateway, the attacker will lose one or more items on the gateway attacker, based on the specific antivirus";
 	
 	public static final String DEEPTHROAT				= "DEEPTHROAT";
 	public static final String DEEPTHROAT_NAME 			= "Deep Throat";
 	public static final int DEEPTHROAT_MAX_LEVEL		= 1;
 	public static final boolean DEEPTHROAT_CUMULATIVE	= false;
 	public static final int DEEPTHROAT_PRICE_PERLEVEL	= 10;
+	public static final String DEEPTHROAT_DESCRIPTION 		= "Equipped on a gateway, if it's attacked it responds by informing the police about the identity of the attacker";
+
 	
 	/* Offensive Software */
 	public static final String SNIFFER					= "SNIFFER";
@@ -104,6 +112,8 @@ public class GameConsts
 	public static final int SNIFFER_MAX_LEVEL			= 3;
 	public static final boolean SNIFFER_CUMULATIVE		= false;
 	public static final int SNIFFER_PRICE_PERLEVEL		= 10;
+	public static final String SNIFFER_DESCRIPTION 		= "It allows to reveal a number of items equipped based on level on the attacked gateway";
+
 	
 	public static final String BRUTEFORCER				= "BRUTEFORCER";
 	public static final String BRUTEFORCER_NAME 		= "Brutus";
@@ -111,6 +121,8 @@ public class GameConsts
 	public static final int BRUTEFORCER_MAX_LEVEL		= 3;
 	public static final boolean BRUTEFORCER_CUMULATIVE	= true;
 	public static final int BRUTEFORCER_PRICE_PERLEVEL	= 10;
+	public static final String BRUTEFORCER_DESCRIPTION 		= "Add a value to attack of gateway but it increases the relevance of the traces left along the path";
+
 	
 	public static final String DICTIONARY				= "DICTIONARY";
 	public static final String DICTIONARY_NAME 			= "John The Rapper";
@@ -118,6 +130,7 @@ public class GameConsts
 	public static final int DICTIONARY_MAX_LEVEL		= 3;
 	public static final boolean DICTIONARY_CUMULATIVE	= true;
 	public static final int DICTIONARY_PRICE_PERLEVEL	= 10;
+	public static final String DICTIONARY_DESCRIPTION 		= "";
 	
 	public static final String PROXY					= "PROXY";
 	public static final String PROXY_NAME 				= "Thor Garlic Proxy";
@@ -126,6 +139,7 @@ public class GameConsts
 	public static final int PROXY_MAX_LEVEL				= 3;
 	public static final boolean PROXY_CUMULATIVE		= false;
 	public static final int PROXY_PRICE_PERLEVEL		= 10;
+	public static final String PROXY_DESCRIPTION 		= "Allows to expand the range of possible path";
 	
 	//***********************//
 	
