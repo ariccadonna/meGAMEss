29160c26b9367e6096b5459d23a47bac64f685e9
UncleSam89 <samuele.degiuseppe@gmail.com>
2013-06-05 16:00:29 +0200

    fuck!!!!

--- Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/Quests.java
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/Quests.java
@@ -2,71 +2,21 @@ package sfs2x.extensions.projectsasha.game.entities;
 
 import sfs2x.extensions.projectsasha.game.entities.gateways.Gateway;
 
-public abstract class Quests
+public astract class Quests
 {
-	protected String player;
-	protected String descritption;
-	protected String type;
-	protected String title;
-	protected String goal;
-	
-	//COSTRUCTOR
-  
-	public Quests(String player, String description, String title, String goal)
-	{
-		this.player = player;
-		this.descritption = description;
-		this.title = title;
-		this.goal = goal;
-	}
 
-	//GETTERS
-  
-	public String getPlayer()
-	{
-		return this.player;
-	}
+	protected String Descritption;
+	protected String Type;
+	protected boolean Goal_reached;
+	protected String Title;
 	
-	public String getDescription()
-	{
-		return this.descritption;
-	}
+	
+	
 
-	public String getTitle()
-	{
-		return this.title;
-	}
 
-	public String getGoal()
-	{
-		return this.goal;
-	}
-	
-	public String getType()
-	{
-		return this.type;
-	}
-	
-	//SETTER
-	
-	public void setType(String type)
-	{
-		this.type = type;
-	}
-	
-	//METHOD
-	
-	public boolean achieve(Gateway target)
-	{
-		if(goal.equals(target.getName()) && (this.player.equals(target.getOwner().getName())))
-			return true;
-		return false;
-	}
+
+
+
+
 
 }
-
-
-
-
-
-
