814875dcebad0224cbe71ab9a2dc5c91a1d2c448
Trulsh <trulsh88@gmail.com>
2013-06-05 17:09:11 +0200

    Add Steal.java and update Quest.java and Conquer.java

--- Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/Quests.java
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/Quests.java
@@ -4,6 +4,42 @@ import sfs2x.extensions.projectsasha.game.entities.gateways.Gateway;
 
 public astract class Quests
 {
+<<<<<<< HEAD
+=======
+	protected String player;
+	protected String descritption;
+	protected String type;
+	protected String title;
+	protected String goal;
+	protected int reward;
+	
+	//COSTRUCTOR
+  
+	public Quests(String player, String description, String title, String goal)
+	{
+		this.player = player;
+		this.descritption = description;
+		this.title = title;
+		this.goal = goal;
+	}
+
+	//GETTERS
+  
+	public String getPlayer()
+	{
+		return this.player;
+	}
+	
+	public String getDescription()
+	{
+		return this.descritption;
+	}
+
+	public String getTitle()
+	{
+		return this.title;
+	}
+>>>>>>> Add ruba.java and update GameConsts.java, Conquista.java and Quest.java
 
 	protected String Descritption;
 	protected String Type;
@@ -11,6 +47,15 @@ public astract class Quests
 	protected String Title;
 	
 	
+<<<<<<< HEAD
+=======
+	public int getReward()
+	{
+		return this.reward;
+	}
+	
+	//SETTER
+>>>>>>> Add ruba.java and update GameConsts.java, Conquista.java and Quest.java
 	
 
 
