dbab99f3d43ea1a237dad88bea16a2e6f3035292
UncleSam89 <samuele.degiuseppe@gmail.com>
2013-06-05 12:34:12 +0200

    Create Quests.java

---
+++ Project_SASHA_Server/src/sfs2x/extensions/projectsasha/game/entities/Quests.java
@@ -0,0 +1,22 @@
+package sfs2x.extensions.projectsasha.game.entities.software;
+
+import sfs2x.extensions.projectsasha.game.entities.gateways.Gateway;
+
+public astract class Quests
+{
+
+  protected String Descritption;
+	protected String Type;
+	protected boolean Goal_reached;
+	protected String Title;
+	
+	
+	
+
+
+
+
+
+
+
+}
